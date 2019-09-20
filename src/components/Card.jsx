a0a973745f3123408503196feb35b1344d49cd4d
William Andree <williamandree@gmail.com>
2019-09-19 19:52:44 -0500

    added react-spring Trails effect on landing title

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,26 @@
 import React from "react";
-import { useSpring, animated } from "react-spring";
+import { useTrail, useSpring, animated } from "react-spring";
 import "../styles/landing.css";
 
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
 const trans1 = (x, y) => `translate3d(${x / 50}px,${y / 50 - 85}px,0)`;
 const trans2 = (x, y) => `translate3d(${x / 45 + 25}px,${y / 45 - 130}px,0)`;
-const trans3 = (x, y) => `translate3d(${x / 40 - 195}px,${y / 40 - 140}px,0)`;
+const trans3 = (x, y) => `translate3d(${x / 29 - 195}px,${y / 29 - 140}px,0)`;
 const trans4 = (x, y) => `translate3d(${x / 40 + 98}px,${y / 40 - 10}px,0)`;
-
+const items = ["Max", "ROSEN"];
+const config = { mass: 5, tension: 2000, friction: 500 };
 function Card() {
   const [props, set] = useSpring(() => ({
     xy: [0, 0],
     config: { mass: 10, tension: 550, friction: 140 }
   }));
+  const trail = useTrail(items.length, {
+    config,
+    delay: 700,
+    opacity: 1,
+    yy: 0,
+    from: { opacity: 0, yy: 20 }
+  });
   return (
     <div
       className="container2"
@@ -51,8 +59,18 @@ function Card() {
       </animated.div>
       <div className="headerWrapper">
         <header className="landingHeader">
-          <span className="landingMax">Max</span>
-          <span className="landingRosen">ROSEN</span>
+          {trail.map(({ yy, ...rest }, index) => (
+            <animated.div
+              key={items[index]}
+              className={"landing" + items[index]}
+              style={{
+                ...rest,
+                transform: yy.interpolate(yy => `translate3d(0,${yy}px,0)`)
+              }}
+            >
+              <animated.div>{items[index]}</animated.div>
+            </animated.div>
+          ))}
         </header>
       </div>
       <animated.div
